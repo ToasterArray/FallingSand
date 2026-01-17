edd9910cc07d26c59ec18f5e678015e7887183df
ToasterArray <teo.skorpil@gmail.com>
2026-01-17 22:48:12 +0100

    Sand and stone and commenting, next water and material selection

--- FallingSand.py
+++ FallingSand.py
@@ -6,7 +6,7 @@ import copy
 import os
 import random
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (460,30)
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (460,30)  # sets window position
 SANDCOLOR = (212, 179, 61)
 STONECOLOR = (41, 47, 52)
 SCREENSIZE = WIDTH, HEIGHT = 1000, 1000
@@ -15,64 +15,69 @@ ROWS = WIDTH // CELL_S
 COLS = HEIGHT // CELL_S
 surf = pygame.display.set_mode(SCREENSIZE, 0, -5)
 pygame.display.set_caption("Sand")
-CELLS = np.zeros((ROWS, COLS))
-clock = pygame.time.Clock()
+CELLS = np.zeros((ROWS, COLS))  # creates a 2D list of zeros for storing particle data
+clock = pygame.time.Clock()  # creates a clock for measuring fps
 
 # materials:
 sand = 1
 stone = 2
 
-def main():
-    global CELLS
+# main game loop
+def main(cells):
     pygame.init()
     pygame.font.init()
     text_font = pygame.font.SysFont('Consolas', 30)
     pygame.mouse.set_visible(False)
     surf.fill('black')
     while True:
-        
-        surf.fill('black')
+        surf.fill('black')  # prevents fps counter text from writing on top of itself
+#region FPS counter       
         clock.tick()
         text_surface = text_font.render(f'FPS: {round(clock.get_fps(), 1)}', False, 'white')
-        checkEvents()
-        CELLS = update_sand(surf, CELLS)
         surf.blit(text_surface, (0,0))
+#endregion
+        checkEvents()
+        cells = update_sand(surf, cells)
+#region Temp Brush Handler        
         if pygame.mouse.get_pressed()[0]:
-            add_pixel(CELLS, pygame.mouse.get_pos(), surf, sand)
+            add_pixel(cells, pygame.mouse.get_pos(), surf, sand)
         elif pygame.mouse.get_pressed()[2]:
-            remove_pixel(CELLS, pygame.mouse.get_pos(), surf)
+            remove_pixel(cells, pygame.mouse.get_pos(), surf)
         elif pygame.mouse.get_pressed()[1]:
-            add_pixel(CELLS, pygame.mouse.get_pos(), surf, stone)
+            add_pixel(cells, pygame.mouse.get_pos(), surf, stone)
         draw_brush(surf, pygame.mouse.get_pos())
+#endregion        
         pygame.display.update()
 
+# particle updater
 def update_sand(screen, cells):
-    updated_cells = copy.deepcopy(cells)
-    direction = random.choice(['left', 'right'])
+    updated_cells = copy.deepcopy(cells)  # buffer for updating cells
+    direction = random.choice(['left', 'right'])  # random choice direction for sand piles
     for row in range(cells.shape[0]):
-        if sum(cells[row]) == 0:
+        if sum(cells[row]) == 0:  # optimisation - skips line if there are no particles present in it
             continue
         for col in range(cells.shape[1]):
             if cells[row][col] == sand:   
-                if row + 1 < ROWS and cells[row + 1][col] == 0:
+                if row + 1 < ROWS and cells[row + 1][col] == 0:  # fall straight down
                     updated_cells[row][col] = 0
                     updated_cells[row + 1][col] = 1
-                elif row + 1 < ROWS and col - 1 >= 0 and cells[row + 1][col - 1] == 0 and direction == 'left':
+                elif row + 1 < ROWS and col - 1 >= 0 and cells[row + 1][col - 1] == 0 and direction == 'left':  # fall left
                     updated_cells[row][col] = 0
                     updated_cells[row + 1][col - 1] = 1
-                elif row + 1 < ROWS and col + 1 < COLS and cells[row + 1][col + 1] == 0 and direction == 'right':
+                elif row + 1 < ROWS and col + 1 < COLS and cells[row + 1][col + 1] == 0 and direction == 'right':  # fall right
                     updated_cells[row][col] = 0
                     updated_cells[row + 1][col + 1] = 1
-            if cells[row, col] == 0:
+            if cells[row, col] == 0:  # empty
                 color = 'black'
-            elif cells[row, col] == sand:
+            elif cells[row, col] == sand:  # sand
                 color = SANDCOLOR
-            else:
+            else:  # stone
                 color = STONECOLOR
             # Draw the cell
             pygame.draw.rect(screen, color, (col * CELL_S, row * CELL_S, CELL_S, CELL_S))
     return updated_cells
 
+# adds particle on mouse position
 def add_pixel(cells, position, screen, mat):
     x = round(position[0] / 10)
     y = round(position[1] / 10)
@@ -83,7 +88,7 @@ def add_pixel(cells, position, screen, mat):
         cells[y, x] = 2
         pygame.draw.rect(screen, 'white', (x * CELL_S, y * CELL_S, CELL_S, CELL_S))
 
-
+# removes particle on mouse position
 def remove_pixel(cells, position, screen):
     x = round(position[0] / 10)
     y = round(position[1] / 10)
@@ -91,17 +96,20 @@ def remove_pixel(cells, position, screen):
         cells[y, x] = 0
     pygame.draw.rect(screen, 'black', (x * CELL_S, y * CELL_S, CELL_S, CELL_S))
 
+# TODO: random particle color
 def rand_color(sand_color):
     r, g, b = sand_color
     color = (r + random.randint(-10, 10), g + random.randint(-10, 10), b + random.randint(-10, 10))
     return color
 
+# replaces cursor with square on grid
 def draw_brush(screen, pos):
     x = round(pos[0] / 10)
     y = round(pos[1] / 10)
     pygame.draw.rect(screen, 'white', (x * CELL_S, y * CELL_S, CELL_S, CELL_S))
 
-def checkEvents():  # event handler
+# event handler
+def checkEvents():
     global RUNNING
     global GEN
     for event in pygame.event.get():
@@ -117,4 +125,4 @@ def checkEvents():  # event handler
                 pass'''
 
 if __name__ == '__main__':
-    main()
+    main(CELLS)
